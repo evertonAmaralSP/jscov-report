207360dd6420d19eb12ea08a50b4f6ff628312f7
Everton Amaral <everton.amaral@abril.com.br>
2014-02-04 11:13:29 -0200

    merge

---
+++ tasks/jscov_report.js
@@ -0,0 +1,50 @@
+/*
+ * jscov-report
+ * https://github.com/evertonAmaralSP/jscov-report
+ *
+ * Copyright (c) 2014 Everton Amaral
+ * Licensed under the Apache, v2, License licenses.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('jscov_report', 'Gerar relatorios usando o jscov.jar', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
